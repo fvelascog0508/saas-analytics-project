53321f183fbf6a8fb2ab76374479029dc430b363
Federico Velasco <fvelascog0508@gmail.com>
2026-06-08 09:06:56 +0200

    Add secrets support for deploy

--- app/app.py
+++ app/app.py
@@ -14,7 +14,8 @@ from google import genai
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key = os.getenv("GOOGLE_API_KEY") or st.secrets.get("GOOGLE_API_KEY")
+# api_key = os.getenv("GOOGLE_API_KEY")
 
 if not api_key:
     st.error("❌ API key not loaded")
